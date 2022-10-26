66591f29e20e70b2aad1a6a218bb7062c98c7098
Dharshan-B <109328490+Dharshan-B@users.noreply.github.com>
2022-10-26 19:31:32 +0530

    Update PRINT_THE_DUPLICATE_VALUES_IN_AN_ARRAY.java

--- PRINT_THE_DUPLICATE_VALUES_IN_AN_ARRAY.java
+++ PRINT_THE_DUPLICATE_VALUES_IN_AN_ARRAY.java
@@ -1,8 +1,8 @@
 class PRINT_THE_DUPLICATE_VALUES_IN_AN_ARRAY{
   public static void main(String[] args){
-    int[] ar ={20,50,50,30,40,40,50,34} 
-    for(int i=0;i<ar.lenght-1;i++){
-      for(int j=i+1;j<ar.lenght-1;i++){
+    int[] ar ={20,50,40,50,34} ;
+    for(int i=0;i<=6;i++){
+      for(int j=i+1;j<=6;i++){
         if((ar[i] == ar[j]) && (i!=j))
         {
          System.out.println("Duplicate Element is :"+ ar[j]); 
